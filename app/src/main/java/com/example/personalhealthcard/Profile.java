fd169a321ff5d9d44fcf8aac9d458151c592c655
Vardan08 <vardan.avetisyan2000@gmail.com>
2024-01-17 19:56:11 +0400

    add log out button

--- app/src/main/java/com/example/personalhealthcard/Profile.java
+++ app/src/main/java/com/example/personalhealthcard/Profile.java
@@ -1,19 +1,35 @@
 package com.example.personalhealthcard;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
 
 public class Profile extends Fragment {
 
+    Button logOutButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_doctor_profil, container, false);
+        View view = inflater.inflate(R.layout.fragment_doctor_profil, container, false);
 
+        logOutButton = view.findViewById(R.id.logOut);
 
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+
+        return view;
+    }
+
+    private void openLoginActivity() {
+        Intent intent = new Intent(getActivity(), LoginPage.class);
+        startActivity(intent);
     }
 }
