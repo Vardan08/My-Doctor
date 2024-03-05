73f1d50eeb08451a6caaec6ef037947b04e1dad4
Vardan08 <vardan.avetisyan2000@gmail.com>
2024-03-05 20:00:45 +0400

    forgot password

--- app/src/main/java/com/example/mydoctor/forgot_password_classes/ForgotPassword.java
+++ app/src/main/java/com/example/mydoctor/forgot_password_classes/ForgotPassword.java
@@ -1,7 +1,9 @@
 package com.example.mydoctor.forgot_password_classes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,44 +11,71 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.mydoctor.LoginPage;
 import com.example.mydoctor.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
-    Button buttonSendCode;
-    EditText editTextPhoneNumber;
+    Button forgotButton;
+    EditText editTextEmail;
+    ProgressDialog dialog;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
-        buttonSendCode = findViewById(R.id.buttonSendCode);
-        editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+        forgotButton = findViewById(R.id.buttonSendCode);
+        editTextEmail = findViewById(R.id.email);
+        auth = FirebaseAuth.getInstance();
+        dialog = new ProgressDialog(ForgotPassword.this);
+        dialog.setCancelable(false);
+        dialog.setMessage("Loading.....");
 
-        buttonSendCode.setOnClickListener(new View.OnClickListener() {
+        forgotButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Get user input from EditText field
-                String email = editTextPhoneNumber.getText().toString().trim();
-
-                // Validate email format using a regular expression
-                if (!isValidEmail(email)) {
-                    Toast.makeText(ForgotPassword.this, "Please enter a valid email address", Toast.LENGTH_SHORT).show();
-                    return; // Stop further execution if email is not valid
+                forgotPassword();
+            }
+        });
+    }
+    private void forgotPassword(){
+        if(!isValidEmail()){
+            return;
+        }
+        dialog.show();
+        auth.sendPasswordResetEmail(editTextEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                dialog.dismiss();
+                if(task.isSuccessful()){
+                    openLoginPage();
+                    finish();
+                    Toast.makeText(ForgotPassword.this, "Please Check your email Address!", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(ForgotPassword.this, "Enter correct email Id", Toast.LENGTH_SHORT).show();
                 }
-
-                openPassVerification();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ForgotPassword.this, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private boolean isValidEmail(String email) {
+    private boolean isValidEmail() {
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        String email = editTextEmail.getText().toString();
         return email.matches(emailPattern);
     }
 
-    private void openPassVerification(){
-        Intent intent = new Intent(this, PasswordVerification.class);
+    private void openLoginPage(){
+        Intent intent = new Intent(this, LoginPage.class);
         startActivity(intent);
     }
 }
